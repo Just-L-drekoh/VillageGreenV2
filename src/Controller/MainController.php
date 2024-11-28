6f32efb48e0517714142d7a6bce47e04de2d15b7
logan <loloherbo.drekoh@gmail.com>
2024-11-28 16:39:12 +0100

    update code & rubrics

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Rubric;
+use App\Repository\RubricRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
-    
+
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig', []);
+    }
+
+    public function rubrics(EntityManagerInterface $entityManager): Response
+    {
+
+        try {
+            $viewRubrics = $entityManager->getRepository(Rubric::class)->findBy(['parent' => null]);
+        } catch (\Exception $e) {
+            $this->addFlash('error', 'Impossible de charger les rubriques');
+            return $this->redirectToRoute('app_index');
+        }
+
+        return $this->render('main/rubrics.html.twig', ['rubrics' => $viewRubrics]);
     }
 }
